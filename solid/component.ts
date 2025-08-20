6de6327e795493f3bdd18df4d6aa0660cea5387f
Masato Yuna <masato.yuna1@gmail.com>
2025-08-20 23:29:59 +0900

    updae

---
+++ solid/component.ts
@@ -0,0 +1,5 @@
+export { default as Loading } from "./features/loading/Loading.tsx";
+export { default as Modal } from "./features/modal/Modal.tsx";
+export { default as Notifications } from "./features/notification/Notifications.tsx";
+export { default as ErrorMessage } from "./features/error/ErrorMessage.tsx";
+
