0321e0e9ab916551e3e126411e9d3785cffd9fb6
Masato Yuna <masato.yuna1@gmail.com>
2025-08-21 15:42:53 +0900

    delete unuse file

--- solid/component.ts
+++
@@ -1,5 +0,0 @@
-// deprecated: use direct feature paths instead
-export { Loading } from './features/loading/components/Loading.tsx';
-export { Modal } from './features/modal/components/Modal.tsx';
-export { Notifications } from './features/notification/components/Notifications.tsx';
-export { ErrorMessage } from './features/error/components/ErrorMessage.tsx';
--- vue/component.ts
+++
@@ -1,5 +0,0 @@
-// deprecated: use direct feature paths instead
-export { default as Loading } from './features/loading/components/Loading.tsx';
-export { default as Modal } from './features/modal/components/Modal.tsx';
-export { default as Notifications } from './features/notification/components/Notifications.tsx';
-export { default as ErrorMessage } from './features/error/components/ErrorMessage.tsx';
