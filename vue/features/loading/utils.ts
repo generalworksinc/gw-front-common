4e1df3fe48979fab6cf594e64e1825f832544af5
Masato Yuna <masato.yuna1@gmail.com>
2025-08-30 17:44:17 +0900

    format/bump ver

--- vue/features/loading/utils.ts
+++ vue/features/loading/utils.ts
@@ -4,48 +4,46 @@ type AsyncFunction = (...args: any[]) => Promise<any>;
 type SyncFunction = (...args: any[]) => any;
 
 export async function eventWithLoading(
-  func: AsyncFunction | SyncFunction,
-  ...params: any[]
+	func: AsyncFunction | SyncFunction,
+	...params: any[]
 ): Promise<any> {
-  const loading = useLoading();
-  if (loading.isLoading.value) {
-    return false;
-  }
-  loading.startLoading();
+	const loading = useLoading();
+	if (loading.isLoading.value) {
+		return false;
+	}
+	loading.startLoading();
 
-  return await new Promise<any>((resolve, reject) => {
-    setTimeout(() => {
-      try {
-        const result = func(...params);
-        if (
-          result instanceof Promise ||
-          (result &&
-            typeof (result as any).then === 'function' &&
-            typeof (result as any).catch === 'function')
-        ) {
-          (result as Promise<any>)
-            .then((res) => {
-              loading.stopLoading();
-              resolve(Promise.resolve(res));
-            })
-            .catch((err) => {
-              loading.stopLoading();
-              resolve(Promise.reject(err));
-            });
-        } else {
-          loading.stopLoading();
-          resolve(result);
-        }
-      } catch (ex) {
-        loading.stopLoading();
-        reject(ex);
-      }
-    }, 1);
-  });
+	return await new Promise<any>((resolve, reject) => {
+		setTimeout(() => {
+			try {
+				const result = func(...params);
+				if (
+					result instanceof Promise ||
+					(result &&
+						typeof (result as any).then === 'function' &&
+						typeof (result as any).catch === 'function')
+				) {
+					(result as Promise<any>)
+						.then((res) => {
+							loading.stopLoading();
+							resolve(Promise.resolve(res));
+						})
+						.catch((err) => {
+							loading.stopLoading();
+							resolve(Promise.reject(err));
+						});
+				} else {
+					loading.stopLoading();
+					resolve(result);
+				}
+			} catch (ex) {
+				loading.stopLoading();
+				reject(ex);
+			}
+		}, 1);
+	});
 }
 
 export const awaitLoadingWith = (asyncFn: () => Promise<void>) => {
-  return async () => await eventWithLoading(asyncFn);
+	return async () => await eventWithLoading(asyncFn);
 };
-
-
