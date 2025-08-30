ef5189c64d2154be249a339670f9414fea91d9ea
Masato Yuna <masato.yuna1@gmail.com>
2025-08-30 17:43:17 +0900

    update

--- dist/vue/mod.js
+++ dist/vue/mod.js
@@ -1,10 +1,27 @@
-function v() {
-  const e = { value: !1 }, l = (u) => {
+function r() {
+  const e = { value: !1 }, a = (u) => {
     e.value = u;
   };
-  return { isLoading: e, setLoading: l, startLoading: () => l(!0), stopLoading: () => l(!1) };
+  return { isLoading: e, setLoading: a, startLoading: () => a(!0), stopLoading: () => a(!1) };
 }
-const o = () => ({
+async function f(e, ...a) {
+  const i = r();
+  return i.isLoading.value ? !1 : (i.startLoading(), await new Promise((s, u) => {
+    setTimeout(() => {
+      try {
+        const n = e(...a);
+        n instanceof Promise || n && typeof n.then == "function" && typeof n.catch == "function" ? n.then((l) => {
+          i.stopLoading(), s(Promise.resolve(l));
+        }).catch((l) => {
+          i.stopLoading(), s(Promise.reject(l));
+        }) : (i.stopLoading(), s(n));
+      } catch (n) {
+        i.stopLoading(), u(n);
+      }
+    }, 1);
+  }));
+}
+const v = (e) => async () => await f(e), o = () => ({
   isOpen: !1,
   isConfirm: !1,
   html: "",
@@ -20,39 +37,41 @@ const o = () => ({
   yesFunc: null,
   noFunc: null
 });
-function t(e) {
+function c(e) {
   return typeof e == "function";
 }
-function f() {
+function m() {
   const e = {
     value: o()
-  }, l = (n) => {
-    e.value.isOpen = !0, e.value.isConfirm = !1, e.value.message = n?.message ?? "", e.value.html = n?.html ?? "", e.value.height = n?.height ?? "", e.value.width = n?.width ?? "", e.value.maxHeight = n?.maxHeight ?? "", e.value.maxWidth = n?.maxWidth ?? "", e.value.minHeight = n?.minHeight ?? "", e.value.minWidth = n?.minWidth ?? "", e.value.isScrollY = n?.isScrollY ?? !1, e.value.isScrollX = n?.isScrollX ?? !1, e.value.yesFunc = t(n?.yesFunc) ? n?.yesFunc : () => null, e.value.noFunc = null;
-  }, s = (n) => {
-    l(n), e.value.isConfirm = !0, e.value.noFunc = t(n?.noFunc) ? n?.noFunc : () => null;
-  }, c = () => {
-    t(e.value.yesFunc) && e.value.yesFunc(), i();
+  }, a = (t) => {
+    e.value.isOpen = !0, e.value.isConfirm = !1, e.value.message = t?.message ?? "", e.value.html = t?.html ?? "", e.value.height = t?.height ?? "", e.value.width = t?.width ?? "", e.value.maxHeight = t?.maxHeight ?? "", e.value.maxWidth = t?.maxWidth ?? "", e.value.minHeight = t?.minHeight ?? "", e.value.minWidth = t?.minWidth ?? "", e.value.isScrollY = t?.isScrollY ?? !1, e.value.isScrollX = t?.isScrollX ?? !1, e.value.yesFunc = c(t?.yesFunc) ? t?.yesFunc : () => null, e.value.noFunc = null;
+  }, i = (t) => {
+    a(t), e.value.isConfirm = !0, e.value.noFunc = c(t?.noFunc) ? t?.noFunc : () => null;
+  }, s = () => {
+    c(e.value.yesFunc) && e.value.yesFunc(), l();
   }, u = () => {
-    t(e.value.noFunc) && e.value.noFunc(), i();
-  }, a = () => {
-    t(e.value.yesFunc) && e.value.yesFunc(), i();
-  }, i = () => {
+    c(e.value.noFunc) && e.value.noFunc(), l();
+  }, n = () => {
+    c(e.value.yesFunc) && e.value.yesFunc(), l();
+  }, l = () => {
     e.value = o();
   };
-  return { state: e, open: l, confirm: s, close: c, yes: a, no: u, reset: i };
+  return { state: e, open: a, confirm: i, close: s, yes: n, no: u, reset: l };
 }
-const r = () => Math.random().toString(36).slice(2);
-function m() {
-  const e = { value: [] }, l = (u) => {
-    e.value = e.value.filter((a) => a.id !== u);
+const d = () => Math.random().toString(36).slice(2);
+function h() {
+  const e = { value: [] }, a = (u) => {
+    e.value = e.value.filter((n) => n.id !== u);
   };
   return { notifications: e, add: (u) => {
-    const a = { id: r(), ...u };
-    e.value = [...e.value, a], a.removeAfter && a.removeAfter > 0 && setTimeout(() => l(a.id), a.removeAfter);
-  }, remove: l, clear: () => e.value = [] };
+    const n = { id: d(), ...u };
+    e.value = [...e.value, n], n.removeAfter && n.removeAfter > 0 && setTimeout(() => a(n.id), n.removeAfter);
+  }, remove: a, clear: () => e.value = [] };
 }
 export {
-  v as useLoading,
-  f as useModal,
-  m as useNotification
+  v as awaitLoadingWith,
+  f as eventWithLoading,
+  r as useLoading,
+  m as useModal,
+  h as useNotification
 };
--- vue/mod.ts
+++ vue/mod.ts
@@ -1,5 +1,6 @@
 // vue entry (feature-based directories)
 // Re-export stores only. Components are exported from "./components" entry.
 export * from './features/loading/loadingStore';
+export * from './features/loading/utils';
 export * from './features/modal/modalStore';
 export * from './features/notification/notificationStore';
