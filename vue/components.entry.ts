63b79825a627af967e7169d465d5096bce277e15
Masato Yuna <masato.yuna1@gmail.com>
2025-09-01 19:35:33 +0900

    update

--- vue/components.entry.ts
+++ vue/components.entry.ts
@@ -1,6 +1,6 @@
 // Build-only entry for Vue components (bundled to JS for consumers)
 
-export { default as ErrorMessage } from './features/error/components/ErrorMessage';
-export { default as Loading } from './features/loading/components/Loading';
-export { default as Modal } from './features/modal/components/Modal';
-export { default as Notifications } from './features/notification/components/Notifications';
+export { default as ErrorMessage } from './features/error/components/ErrorMessage.vue';
+export { default as Loading } from './features/loading/components/Loading.vue';
+export { default as Modal } from './features/modal/components/Modal.vue';
+export { default as Notifications } from './features/notification/components/Notifications.vue';
