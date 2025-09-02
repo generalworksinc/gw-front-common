ea8aae38e72ac46515ac8550085ca4f844e5b3cc
Masato Yuna <masato.yuna1@gmail.com>
2025-09-03 02:15:26 +0900

    update mod

--- vue/mod.ts
+++ vue/mod.ts
@@ -4,4 +4,3 @@ export * from './features/loading/loadingStore';
 export * from './features/loading/utils';
 export * from './features/modal/modalStore';
 export * from './features/notification/notificationStore';
-export { default as NuxtModule } from './nuxt/module';
