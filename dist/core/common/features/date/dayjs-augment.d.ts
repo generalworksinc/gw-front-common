e735ffae46b6d7cf0dccba6c2dcce004584215ab
Masato Yuna <m.yuna@generalworks.co.jp>
2026-01-25 20:58:39 +0900

    update

--- dist/core/common/features/date/dayjs-augment.d.ts
+++ dist/core/common/features/date/dayjs-augment.d.ts
@@ -1,9 +1,9 @@
 declare module 'dayjs' {
-  interface Dayjs {
-    utc(keepLocalTime?: boolean): import('dayjs').Dayjs;
-  }
+	interface Dayjs {
+		utc(keepLocalTime?: boolean): import('dayjs').Dayjs;
+	}
 
-  interface DayjsStatic {
-    utc(date?: string | number | Date, format?: string): import('dayjs').Dayjs;
-  }
+	interface DayjsStatic {
+		utc(date?: string | number | Date, format?: string): import('dayjs').Dayjs;
+	}
 }
