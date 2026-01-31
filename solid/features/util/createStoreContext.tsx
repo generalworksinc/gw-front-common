e48a04ce9f0af97b1d216ee7d8897a0ce0c53748
Masato Yuna <m.yuna@generalworks.co.jp>
2026-01-31 19:04:37 +0900

    fix lint

--- solid/features/util/createStoreContext.tsx
+++ solid/features/util/createStoreContext.tsx
@@ -1,24 +1,27 @@
+/** @jsxImportSource solid-js */
 import { createContext, type ParentComponent, useContext } from 'solid-js';
 
 type StoreContextResult<T> = {
-  Provider: ParentComponent;
-  useStore: () => T;
-  Context: ReturnType<typeof createContext<T | undefined>>;
+	Provider: ParentComponent;
+	useStore: () => T;
+	Context: ReturnType<typeof createContext<T | undefined>>;
 };
 
-export function createStoreContext<T>(createStore: () => T): StoreContextResult<T> {
-  const Ctx = createContext<T>();
+export function createStoreContext<T>(
+	createStore: () => T,
+): StoreContextResult<T> {
+	const Ctx = createContext<T>();
 
-  const Provider: ParentComponent = (props) => {
-    const store = createStore();
-    return <Ctx.Provider value={store}>{props.children}</Ctx.Provider>;
-  };
+	const Provider: ParentComponent = (props) => {
+		const store = createStore();
+		return <Ctx.Provider value={store}>{props.children}</Ctx.Provider>;
+	};
 
-  const useStore = () => {
-    const v = useContext(Ctx);
-    if (!v) throw new Error('StoreContext must be used within its Provider');
-    return v;
-  };
+	const useStore = () => {
+		const v = useContext(Ctx);
+		if (!v) throw new Error('StoreContext must be used within its Provider');
+		return v;
+	};
 
-  return { Provider, useStore, Context: Ctx };
+	return { Provider, useStore, Context: Ctx };
 }
